927a88171cdd6a4905695a00575209a23bf9aa90
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-07 18:37:03 -0500

    chore: configurar entornos y variables de aplicacion

--- src/test/java/com/motorepuestos/inventario/InventarioApiApplicationTests.java
+++ src/test/java/com/motorepuestos/inventario/InventarioApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.motorepuestos.inventario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InventarioApiApplicationTests {
 
 	@Test
