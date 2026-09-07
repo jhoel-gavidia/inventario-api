0ceee6e6dc16dc0cdb316445e693f12a81f166b0
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-07 11:53:37 -0500

    feat: implementar servicio de usuario

--- src/main/java/com/motorepuestos/inventario/service/UsuarioService.java
+++ src/main/java/com/motorepuestos/inventario/service/UsuarioService.java
@@ -1,4 +1,18 @@
 package com.motorepuestos.inventario.service;
 
+import com.motorepuestos.inventario.DTOs.Request.UsuarioRequest;
+import com.motorepuestos.inventario.DTOs.Response.UsuarioResponse;
+
+import java.util.List;
+
 public interface UsuarioService {
+    UsuarioResponse crear(UsuarioRequest request);
+
+    UsuarioResponse obtenerPorId(Long id);
+
+    List<UsuarioResponse> obtenerTodos();
+
+    UsuarioResponse actualizar(Long id, UsuarioRequest request);
+
+    void eliminar(Long id);
 }
--- src/main/java/com/motorepuestos/inventario/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/motorepuestos/inventario/service/impl/UsuarioServiceImpl.java
@@ -1,4 +1,82 @@
 package com.motorepuestos.inventario.service.impl;
 
-public class UsuarioServiceImpl {
+import com.motorepuestos.inventario.DTOs.Request.UsuarioRequest;
+import com.motorepuestos.inventario.DTOs.Response.UsuarioResponse;
+import com.motorepuestos.inventario.entity.Usuario;
+import com.motorepuestos.inventario.mapper.UsuarioMapper;
+import com.motorepuestos.inventario.repository.UsuarioRepository;
+import com.motorepuestos.inventario.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioMapper usuarioMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UsuarioResponse crear(UsuarioRequest request) {
+
+        if (usuarioRepository.existsByUsername(request.getUsername())) {
+            throw new RuntimeException("El username ya existe");
+        }
+
+        Usuario usuario = usuarioMapper.toEntity(request);
+
+        usuarioRepository.save(usuario);
+
+        return usuarioMapper.toResponse(usuario);
+    }
+
+    @Override
+    public UsuarioResponse obtenerPorId(Long id) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        return usuarioMapper.toResponse(usuario);
+    }
+
+    @Override
+    public List<UsuarioResponse> obtenerTodos() {
+        return usuarioRepository.findAll()
+                .stream()
+                .map(usuarioMapper::toResponse)
+                .toList();
+    }
+
+    @Override
+    public UsuarioResponse actualizar(Long id, UsuarioRequest request) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        if (!usuario.getUsername().equals(request.getUsername())
+                && usuarioRepository.existsByUsername(request.getUsername())) {
+            throw new RuntimeException("El username ya existe");
+        }
+
+        usuario.setUsername(request.getUsername());
+        usuario.setPassword(
+                passwordEncoder.encode(request.getPassword())
+        );
+        usuario.setRol(request.getRol());
+        usuario.setEstado(request.getEstado());
+
+        Usuario usuarioActualizado = usuarioRepository.save(usuario);
+
+        return usuarioMapper.toResponse(usuarioActualizado);
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        usuarioRepository.delete(usuario);
+    }
 }
