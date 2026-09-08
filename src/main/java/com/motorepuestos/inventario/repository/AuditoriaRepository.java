77ae1b489022a382c7b7edaa8230570eb241286d
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-08 10:30:55 -0500

    fix: AuditoriaRepository.findByEntidadAndEntidadId usa @EntityGraph para cargar 'usuario' eager, evitando LazyInitializationException al leer auditoria.getUsuario().getUsername() en el assert

--- src/main/java/com/motorepuestos/inventario/repository/AuditoriaRepository.java
+++ src/main/java/com/motorepuestos/inventario/repository/AuditoriaRepository.java
@@ -1,9 +1,14 @@
 package com.motorepuestos.inventario.repository;
 
 import com.motorepuestos.inventario.entity.Auditoria;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
 
+    @EntityGraph(attributePaths = {"usuario"})
+    Optional<Auditoria> findByEntidadAndEntidadId(String entidad, Long entidadId);
 
 }
---
+++ src/test/java/com/motorepuestos/inventario/support/AbstractIntegrationTest.java
@@ -0,0 +1,4 @@
+package com.motorepuestos.inventario.support;
+
+public class AbstractIntegrationTest {
+}
