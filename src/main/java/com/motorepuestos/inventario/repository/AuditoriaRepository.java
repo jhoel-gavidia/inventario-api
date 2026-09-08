9f38d2231cf7e2903d030c3bb95a6423566cf42b
jhoel-gavidia <jhoel.gavidia01@gmail.com>
2026-09-07 20:26:46 -0500

    ci: test 4

--- src/main/java/com/motorepuestos/inventario/repository/AuditoriaRepository.java
+++ src/main/java/com/motorepuestos/inventario/repository/AuditoriaRepository.java
@@ -4,4 +4,6 @@ import com.motorepuestos.inventario.entity.Auditoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
+
+
 }
